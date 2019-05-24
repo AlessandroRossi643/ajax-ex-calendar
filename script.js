c586ed3717c253c5107d7891dd053f858d0e57f5
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-05-24 20:47:57 +0200

    1. Milestone 1 corretta

--- script.js
+++ script.js
@@ -1,28 +1,58 @@
 $(document).ready(function(){
+  var source=$('#template_calendar').html();
+  var template=Handlebars.compile(source);
 
-  var template_calendar=$('#template_calendar').html();
-  var template_function=Handlebars.compile(template_calendar)
+  var mindate=moment("2018-01-01");
+  var maxdate=moment("2018-12-01");
 
-  var date = '2018-01-01';
-  var moment_date = moment(date);
+  var dataattuale=moment("2018-01-01")
+  generaMese(dataattuale);
 
-  var mese= moment_date.format("MMMM");
-  var anno= moment_date.format("YYYY")
-  var mese_anno= mese+' '+anno;
+  $('.next').click(function(){
+    console.log(dataattuale.format('MMMM'))
 
-  var giorni=moment_date.daysInMonth();
-  console.log(giorni);
+    if (dataattuale.isSameOrAfter(maxdate)) {
+      $(this).attr("disabled",true);
+    }
+    else {
+      dataattuale.add(1,'months');
+      generaMese(dataattuale);
+      $(this).attr("disabled",false);
+    }
+  });
 
-  for (var i = 1; i <= giorni.length; i++) {
+  $('.prev').click(function(){
+    console.log(dataattuale.format('MMMM'))
 
+    if (dataattuale.isSameOrBefore(mindate)) {
+      $(this).attr("disabled",true);
+    }
+    else {
+      dataattuale.subtract(1,'months');
+      generaMese(dataattuale);
+      $(this).attr("disabled",false);
+    }
+  });
+
+
+
+  function generaMese(giornoattuale){
+
+    $('#giorni_mesecorrente').html('');
+    var meseattuale=giornoattuale.format('MMMM');
+    var annoattuale=giornoattuale.format('YYYY');
+
+    var giorniMese= giornoattuale.daysInMonth();
+    $('#mese_corrente').text(meseattuale + " " + annoattuale);
+
+    for (var i = 1; i <= giorniMese; i++) {
+      var giorno= i;
+      var context={
+        'giorni': giorno + " " + meseattuale
+      }
+      $('#giorni_mesecorrente').append(template(context));
+    }
   }
-  console.log(giorno)
 
-  var placeholder={
-    'mese': mese_anno
-  }
-
-  var html=template_function(placeholder);
-  $('.container_mesi').append(html);
 
 });

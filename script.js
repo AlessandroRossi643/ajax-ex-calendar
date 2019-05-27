f9ffb7db9bf63eabe531d918a5a9c4389049fd95
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-05-27 08:26:56 +0200

    2. Esercizio completato, da risolvere bug e migliorare parte estetica

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
   var dataattuale=moment("2018-01-01")
   generaMese(dataattuale);
 
+
   $('.next').click(function(){
     console.log(dataattuale.format('MMMM'))
 
@@ -37,7 +38,6 @@ $(document).ready(function(){
 
 
   function generaMese(giornoattuale){
-
     $('#giorni_mesecorrente').html('');
     var meseattuale=giornoattuale.format('MMMM');
     var annoattuale=giornoattuale.format('YYYY');
@@ -48,11 +48,54 @@ $(document).ready(function(){
     for (var i = 1; i <= giorniMese; i++) {
       var giorno= i;
       var context={
-        'giorni': giorno + " " + meseattuale
+        'giorni': giorno + " " + meseattuale,
+        'giornoiso': giornoattuale.format('YYYY-MM-')+ addzero(i)
       }
       $('#giorni_mesecorrente').append(template(context));
     }
+    var mesenumero= giornoattuale.format('M');
+    trovaFestivita(mesenumero);
   }
 
+  function addzero(giorno){
+    if (giorno<10) {
+      return "0" + giorno;
+    }
+    else {
+      return giorno;
+    }
+  }
 
+  function trovaFestivita(mesescelto){
+
+    $.ajax({
+      url:"https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0.",
+      method:"GET",
+      data:{
+        'year': 2018,
+        'month': mesescelto - 1
+      },
+      success: function(data){
+        var result= data.response;
+        console.log(result);
+
+        for (var i = 0; i < result.length; i++) {
+          var datafesta=result[i].date;
+          var nomefesta=result[i].name;
+        }
+
+
+        $('li[data-giorno-iso]').each(function(){
+          if ($(this).attr('data-giorno-iso')==datafesta) {
+            var giornofesta= $(this).text();
+            $(this).text(giornofesta+ " "+nomefesta)
+            $(this).addClass("red");
+          }
+        });
+      },
+      error: function(){
+        alert("Ops, qualcosa è andato storto");
+      }
+    });
+  }
 });
